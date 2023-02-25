687be87a5caede5b474b755f30871d2025723ac6
RiniCatherineJoseph <rj373@njit.edu>
2023-02-25 18:08:52 -0500

    landing screen UI - Rini

--- client/crudnews_client/src/App.js
+++ client/crudnews_client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import LandingScreen from "./LandingScreen";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [username, setUsername] = useState("Rini Joseph");
+
+  const handleLogin = (username) => {
+    setLoggedIn(true);
+    setUsername(username);
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+    setUsername("");
+  };
+
+  if (loggedIn) {
+    return <LandingScreen username={username} onLogout={handleLogout} />;
+  } else {
+    return <LandingScreen username={username} onLogout={handleLogout} />;
+  }
 }
 
 export default App;
