2b7d5ed7b19a98babd4317a3b5ea7fe0f6fc9cc1
chaitanyasagar3 <90219296+chaitanyasagar3@users.noreply.github.com>
2023-03-05 17:42:52 -0500

    merge error fix

--- server/app.js
+++ server/app.js
@@ -1,16 +1,15 @@
 // Import all dependencies & middleware here
-import express from 'express';
-import bodyParser from 'body-parser';
-import {userController} from './controller/index.js';
+import express from "express";
+import bodyParser from "body-parser";
+import { userController } from "./controller/index.js";
 import mongoose from "mongoose";
-import cors from 'cors';
-
+import cors from "cors";
 
 // Init an Express App.
 const app = express();
 
 // Use your dependencies here
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -19,10 +18,8 @@ app.use("/", userController);
 
 // Start Server here
 app.listen(8080, () => {
-   console.log('Server is running on port 8080!');
-   mongoose.connect("mongodb://localhost/test").then(() => {
-      console.log(`Conneted to mongoDB at port 27017`);
-    });
+  console.log("Server is running on port 8080!");
+  mongoose.connect("mongodb://localhost/test").then(() => {
+    console.log(`Conneted to mongoDB at port 27017`);
+  });
 });
-
-
--- server/database/schemas/user.schema.js
+++ server/database/schemas/user.schema.js
@@ -1,15 +1,17 @@
-import { Schema } from 'mongoose';
-import sha256 from 'sha256';
-const userSchema = new Schema({
+import { Schema } from "mongoose";
+import sha256 from "sha256";
+const userSchema = new Schema(
+  {
     hashedPassword: { type: String, required: true },
-    username: { type: String , required: true },
-},
-{ 
+    username: { type: String, required: true },
+  },
+  {
     methods: {
-        comparePassword(password) {
-            return this.hashedPassword === sha256(password);
-        }
-    }
-});
+      comparePassword(password) {
+        return this.hashedPassword === sha256(password);
+      },
+    },
+  }
+);
 
 export default userSchema;
