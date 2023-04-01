d82d9a4e9f7165288b5790305793b3958ab882a4
RiniCatherineJoseph <rj373@njit.edu>
2023-04-01 05:12:13 -0400

    updatd with functions to get all the news - RIni

--- client/src/api/news.js
+++ client/src/api/news.js
@@ -11,5 +11,77 @@ export const getGeneralNews = async () => {
   } catch (error) {
     console.error(error);
   }
-  
+};
+export const getBusinessNews = async () => {
+  try {
+    const response = await axios.get("/news", {
+      params: { category: "business" },
+    });
+    const articles = response.data;
+    console.log(articles);
+    return articles;
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const getSportsNews = async () => {
+  try {
+    const response = await axios.get("/news", {
+      params: { category: "sports" },
+    });
+    const articles = response.data;
+    console.log(articles);
+    return articles;
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const getEntertainmentNews = async () => {
+  try {
+    const response = await axios.get("/news", {
+      params: { category: "entertainment" },
+    });
+    const articles = response.data;
+    console.log(articles);
+    return articles;
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const getHealthNews = async () => {
+  try {
+    const response = await axios.get("/news", {
+      params: { category: "health" },
+    });
+    const articles = response.data;
+    console.log(articles);
+    return articles;
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const getScienceNews = async () => {
+  try {
+    const response = await axios.get("/news", {
+      params: { category: "science" },
+    });
+    const articles = response.data;
+    console.log(articles);
+    return articles;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getTechnologyNews = async () => {
+  try {
+    const response = await axios.get("/news", {
+      params: { category: "technology" },
+    });
+    const articles = response.data;
+    console.log(articles);
+    return articles;
+  } catch (error) {
+    console.error(error);
+  }
 };
