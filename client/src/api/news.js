3a307f371cc0c736ae78512227461b2d35f90b72
RiniCatherineJoseph <rj373@njit.edu>
2023-04-02 00:15:55 -0400

    news.js - rini

--- client/src/api/news.js
+++ client/src/api/news.js
@@ -1,82 +1,9 @@
 import axios from "./axios";
 
-export const getGeneralNews = async () => {
+export const getNewsByCategory = async (category) => {
   try {
     const response = await axios.get("/news", {
-      params: { category: "general" },
-    });
-    const articles = response.data;
-    console.log(articles);
-    return articles;
-  } catch (error) {
-    console.error(error);
-  }
-};
-export const getBusinessNews = async () => {
-  try {
-    const response = await axios.get("/news", {
-      params: { category: "business" },
-    });
-    const articles = response.data;
-    console.log(articles);
-    return articles;
-  } catch (error) {
-    console.error(error);
-  }
-};
-export const getSportsNews = async () => {
-  try {
-    const response = await axios.get("/news", {
-      params: { category: "sports" },
-    });
-    const articles = response.data;
-    console.log(articles);
-    return articles;
-  } catch (error) {
-    console.error(error);
-  }
-};
-export const getEntertainmentNews = async () => {
-  try {
-    const response = await axios.get("/news", {
-      params: { category: "entertainment" },
-    });
-    const articles = response.data;
-    console.log(articles);
-    return articles;
-  } catch (error) {
-    console.error(error);
-  }
-};
-export const getHealthNews = async () => {
-  try {
-    const response = await axios.get("/news", {
-      params: { category: "health" },
-    });
-    const articles = response.data;
-    console.log(articles);
-    return articles;
-  } catch (error) {
-    console.error(error);
-  }
-};
-export const getScienceNews = async () => {
-  try {
-    const response = await axios.get("/news", {
-      params: { category: "science" },
-    });
-    const articles = response.data;
-    console.log(articles);
-    return articles;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-export const getTechnologyNews = async () => {
-  try {
-    const response = await axios.get("/news", {
-      params: { category: "technology" },
+      params: { category: category },
     });
     const articles = response.data;
     console.log(articles);
