b13b86beb5654807852c3332274a0d47f153881e
chaitanyasagar3 <90219296+chaitanyasagar3@users.noreply.github.com>
2023-04-04 16:28:16 -0400

    Updated APIs

--- server/controller/news.controller.js
+++ server/controller/news.controller.js
@@ -3,18 +3,59 @@ import NewsAPI from "newsapi";
 
 const newsController = express.Router();
 
-newsController.get("/", async (req, res) => {
+newsController.get("/category/", async (req, res) => {
   try {
     const { category } = req.query;
     console.log(category);
     const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
     const response = await newsapi.v2.everything({
-      q : category,
+      q: category,
       language: "en",
       sortBy: "relevancy",
-      page: 2,
     });
+    response.articles.sort(
+      (a, b) => new Date(b.publishedAt) - new Date(a.publishedAt)
+    );
+    response.articles.slice(0, 250);
     res.json(response.articles);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+newsController.post("/", async (req, res) => {
+  try {
+    const user = req.body.user; // assuming user data is attached to the request object
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
+    const categories = Object.keys(user.preferences).filter(
+      (category) => user.preferences[category]
+    );
+    // categories = req.query.categories.split(",");
+    // const query = categories.join(" OR ");
+
+    const response = await Promise.all(
+      categories.map((category) =>
+        newsapi.v2.everything({
+          q: category,
+          language: "en",
+          sortBy: "publishedAt",
+          page: 1,
+        })
+      )
+    );
+
+    const articles = response
+      .map((res) => res.articles)
+      .reduce((acc, val) => acc.concat(val), [])
+      .sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt))
+      .slice(0, 250);
+    console.log(articles);
+    res.json(articles);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
